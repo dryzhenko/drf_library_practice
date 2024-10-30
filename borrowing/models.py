b622062ec8a08e232780cccf22b2d0b80b3fdd06
Daniil <daniilrizhenko@gmail.com>
2024-10-30 15:00:09 +0200

    add borrowing model

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField()
+    book_id = models.IntegerField()
+    user_id = models.IntegerField()
+
+    def __str__(self):
+        return f"User {self.user_id}, book {self.book_id}"
