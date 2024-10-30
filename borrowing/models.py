71e6110ed4e1b1d333c39b84930561d82fe44e12
Daniil <daniilrizhenko@gmail.com>
2024-10-30 15:58:24 +0200

    change model fields book_id & user_id

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,12 +1,24 @@
 from django.db import models
+from books.models import Book
+from django.conf import settings
 
 
 class Borrowing(models.Model):
     borrow_date = models.DateField()
     expected_return_date = models.DateField()
     actual_return_date = models.DateField()
-    book_id = models.IntegerField()
-    user_id = models.IntegerField()
+    book_id = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name="borrowing",
+        db_column="book_id"
+    )
+    user_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="borrowing",
+        db_column="user_id"
+    )
 
     def __str__(self):
         return f"User {self.user_id}, book {self.book_id}"
